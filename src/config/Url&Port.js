0006896320c78940b02cd2feebdbadcfb3b7118c
Juan diego <juandiroca@hotmail.com>
2025-05-22 19:27:08 -0500

    Se corrijen detalles

--- src/config/Url&Port.js
+++ src/config/Url&Port.js
@@ -1,5 +1,5 @@
 // src/config/Url&Port.js
 // Este archivo contiene la configuración de la URL y el puerto del backend.
 export const backendUrl = 'http://localhost';
-export const backendPort = '3000';
+export const backendPort = '8080';
 export const apiBaseUrl = `${backendUrl}:${backendPort}`;
