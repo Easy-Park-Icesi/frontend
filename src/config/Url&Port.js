4b79902275e42b9f802f5baa92608b7644518cdf
Juan Yustes <yustes2017@gmail.com>
2025-05-23 00:14:47 -0500

    change name of urlport file

--- src/config/Url&Port.js
+++
@@ -1,5 +0,0 @@
-// src/config/Url&Port.js
-// Este archivo contiene la configuración de la URL y el puerto del backend.
-export const backendUrl = 'http://localhost';
-export const backendPort = '8080';
-export const apiBaseUrl = `${backendUrl}:${backendPort}`;
