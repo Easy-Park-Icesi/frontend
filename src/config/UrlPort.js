3aca461474e7ee09acab7ec534c6072b5a452cbd
Juan Diego Rosero <103261154+juandy7@users.noreply.github.com>
2025-05-23 19:18:10 -0500

    Update UrlPort.js

--- src/config/UrlPort.js
+++ src/config/UrlPort.js
@@ -1,5 +1,5 @@
 // src/config/Url&Port.js
 // Este archivo contiene la configuración de la URL y el puerto del backend.
-export const backendUrl = 'http://localhost';
+export const backendUrl = 'http://10.10.3.138';
 export const backendPort = '8080';
 export const apiBaseUrl = `${backendUrl}:${backendPort}`;
